a99fb7b89472a7a2205f223735d6553ba41a9dc5
the96 <aar30px2@gmail.com>
2022-09-08 22:14:23 +0900

    bottomにボタンとか置いてみた

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -10,31 +10,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+      ),
       home: Scaffold(
-          appBar: AppBar(title: const Text("MaterialApp"), actions: const []),
-          body: Column(children: [
-            const Padding(padding: EdgeInsets.only(top: 50)),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              const Padding(padding: EdgeInsets.only(right: 50)),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text('hello'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text('world'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text('column'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text('and row'),
-              ),
-              const Padding(padding: EdgeInsets.only(left: 50)),
-            ])
-          ])),
+        appBar: AppBar(title: const Text("MaterialApp"), actions: const []),
+        body: Column(children: [
+          const Padding(padding: EdgeInsets.only(top: 50)),
+          Text(
+            'Camera App',
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          const Padding(padding: EdgeInsets.only(top: 50)),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            const Padding(padding: EdgeInsets.only(right: 50)),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('hello'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('world'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('column'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text('and row'),
+            ),
+            const Padding(padding: EdgeInsets.only(left: 50)),
+          ])
+        ]),
+        bottomNavigationBar: BottomAppBar(
+            color: Theme.of(context).primaryColor,
+            shape: const CircularNotchedRectangle(),
+            child: Container(height: 50.0)),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () => {},
+            tooltip: 'Open Camera',
+            child: const Icon(Icons.camera)),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
     );
   }
 }
